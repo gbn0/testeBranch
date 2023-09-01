d38b3329ad8d58fc7ce87bd59bb53091a3a9b739
Guilherme Kuhn <guilherme.kuhn002@edu.pucrs.br>
2023-09-01 18:54:21 -0300

    Construtor Revista

--- src/Revista.java
+++ src/Revista.java
@@ -1,5 +1,18 @@
 package src;
 
-public class Revista extends ItensColecionaveis{
-    
+public class Revista extends ItensColecionaveis {
+
+    private int anoPublicacao;
+    private int volume;
+    private String editora;
+    private String assuntos;
+
+    public Revista(int c, String n, String d, int a, int v, String e, String as) {
+
+        // super(c,n,d);
+        this.anoPublicacao = a;
+        this.volume = v;
+        this.editora = e;
+        this.assuntos = as;
+    }
 }
