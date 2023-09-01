9a7da3dd59ff939e06be861896ea6e90da014c44
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-09-01 18:44:33 -0300

    Construtor item colecionavel

---
+++ src/ItensColecionaveis.java
@@ -0,0 +1,27 @@
+package src;
+
+import java.util.ArrayList;
+
+public class ItensColecionaveis {
+    
+    private int codigo;
+    private String nome;
+    private String dataAquisicao;
+    private ArrayList<String> listaAutores;
+    private ArrayList<Livro> livros;
+    private ArrayList<Cd> cds;
+    private ArrayList<Revista> revistas;
+    private ArrayList<Dvd> dvds;
+
+    public ItensColecionaveis(int codigo, String nome, String data) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.dataAquisicao = data;
+        this.listaAutores = new ArrayList<String>();
+        this.livros = new ArrayList<Livro>();
+        this.cds = new ArrayList<Cd>();
+        this.revistas = new ArrayList<Revista>();
+        this.dvds = new ArrayList<Dvd>();
+    }
+
+}
